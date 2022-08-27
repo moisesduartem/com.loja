1be8a56a422c0dab30fff4fdd3ca5c66dbe1aa48
Moisés Duarte <moisesduartem@gmail.com>
2022-08-27 13:47:55 -0300

    chore(entregas): jackson

--- entregas.worker/src/main/java/com/loja/entregas/worker/consumer/PedidoCadastradoConsumer.java
+++ entregas.worker/src/main/java/com/loja/entregas/worker/consumer/PedidoCadastradoConsumer.java
@@ -1,8 +1,12 @@
 package com.loja.entregas.worker.consumer;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.loja.queue.events.PedidoCadastradoEvent;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.utils.SerializationUtils;
+import org.springframework.boot.jackson.JsonObjectSerializer;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +15,10 @@ public class PedidoCadastradoConsumer {
     @RabbitListener(queues = { "pedido_cadastrado" })
     public void cadastrarEntrega(@Payload byte[] body) {
         try {
-            PedidoCadastradoEvent data = (PedidoCadastradoEvent) SerializationUtils.deserialize(body);
-            System.out.println(data);
+            PedidoCadastradoEvent event = (PedidoCadastradoEvent) SerializationUtils.deserialize(body);
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+            System.out.println(mapper.writeValueAsString(event));
         } catch (Exception e) {
             e.printStackTrace();
         }
