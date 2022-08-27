5d4c3112d0de2cc68bf22251e7603ca32222685f
Moisés Duarte <moisesduartem@gmail.com>
2022-08-27 15:05:46 -0300

    feat(pedidos.api/entregas.worker): logs

--- entregas.worker/src/main/java/com/loja/entregas/worker/consumer/PedidoCadastradoConsumer.java
+++ entregas.worker/src/main/java/com/loja/entregas/worker/consumer/PedidoCadastradoConsumer.java
@@ -4,6 +4,8 @@ import com.loja.entregas.worker.entity.Entrega;
 import com.loja.entregas.worker.repository.EntregaRepository;
 import com.loja.entregas.worker.utils.JsonMapper;
 import com.loja.queue.events.PedidoCadastradoEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.utils.SerializationUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +14,9 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class PedidoCadastradoConsumer {
+
+    private Logger logger = LoggerFactory.getLogger(PedidoCadastradoConsumer.class);
+
     @Autowired
     private EntregaRepository entregaRepository;
 
@@ -20,12 +25,20 @@ public class PedidoCadastradoConsumer {
 
     @RabbitListener(queues = {"pedido_cadastrado"})
     public void cadastrarEntrega(@Payload byte[] body) {
+        logger.info("Deserializando mensagem recebida");
         PedidoCadastradoEvent event = (PedidoCadastradoEvent) SerializationUtils.deserialize(body);
 
+        logger.info(
+                "Gerando entrega para pedidoId={} e endereco={}",
+                event.getPedidoId(),
+                event.getEndereco()
+        );
         Entrega entrega = new Entrega(event.getPedidoId(), event.getEndereco());
 
+        logger.info("Tentando salvar entrega no banco de dados");
         entregaRepository.save(entrega);
 
-        System.out.println(jsonMapper.mapToJsonString(entrega));
+        logger.info("Entrega cadastrada com sucesso, id={}", entrega.getId());
+        logger.info(jsonMapper.mapToJsonString(entrega));
     }
 }
--- pedidos.api/src/main/java/com/loja/pedidos/api/service/PedidoService.java
+++ pedidos.api/src/main/java/com/loja/pedidos/api/service/PedidoService.java
@@ -9,6 +9,8 @@ import com.loja.pedidos.api.repository.ClienteRepository;
 import com.loja.pedidos.api.repository.PedidoRepository;
 import com.loja.pedidos.api.repository.ProdutoRepository;
 import com.loja.queue.events.PedidoCadastradoEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.utils.SerializationUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,6 +20,8 @@ import java.util.List;
 
 @Service
 public class PedidoService {
+
+    private Logger logger = LoggerFactory.getLogger(PedidoService.class);
 
     @Autowired
     private PedidoRepository pedidoRepository;
@@ -30,21 +34,34 @@ public class PedidoService {
     private QueueSender queueSender;
 
     public Pedido cadastrarPedido(CadastrarPedidoDto cadastroDto) {
+        logger.info("Buscando cliente com id={}", cadastroDto.getClienteId());
         Cliente cliente = clienteRepository.getReferenceById(cadastroDto.getClienteId());
+
+        logger.info("Buscando produtos com ids={}", cadastroDto.getProdutosIds());
         List<Produto> produtos = produtoRepository.findAllById(cadastroDto.getProdutosIds());
 
         Pedido pedido = new Pedido(cliente, new HashSet<>(produtos));
 
+        logger.info("Tentando salvar pedido no banco de dados");
         pedidoRepository.save(pedido);
 
+        logger.info("Pedido cadastrado com sucesso, id={}", pedido.getId());
         PedidoCadastradoEvent event = new PedidoCadastradoEvent(
                 pedido.getId(),
                 cadastroDto.getEndereco().trim().toUpperCase());
 
+        logger.info(
+                "Serializando evento com pedidoId={} e endereco={}",
+                event.getPedidoId(),
+                event.getEndereco()
+        );
         byte[] data = SerializationUtils.serialize(event);
 
+        logger.info("Enviando evento de pedidoId={} para fila", event.getPedidoId());
         queueSender.send("pedido_cadastrado", data);
 
+        logger.info("Evento de criação do pedido enviado para fila com sucesso");
+        logger.info("Retornando pedido criado");
         return pedido;
     }
 
