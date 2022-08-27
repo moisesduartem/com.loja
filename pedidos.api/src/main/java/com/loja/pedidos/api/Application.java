2103d0e1585415c526707268ecf6b6b85f0ec911
Moisés Duarte <moisesduartem@gmail.com>
2022-08-27 14:29:56 -0300

    feat: disponbiliza h2 em porta tcp

--- pedidos.api/src/main/java/com/loja/pedidos/api/Application.java
+++ pedidos.api/src/main/java/com/loja/pedidos/api/Application.java
@@ -3,6 +3,10 @@ package com.loja.pedidos.api;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.h2.tools.Server;
+
+import java.sql.SQLException;
 
 @EnableRabbit
 @SpringBootApplication
@@ -12,4 +16,9 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean(initMethod = "start", destroyMethod = "stop")
+	public Server inMemoryH2DatabaseServer() throws SQLException {
+		return Server.createTcpServer(
+				"-tcp", "-tcpAllowOthers", "-tcpPort", "9090");
+	}
 }
